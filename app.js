0d8139552a0bc1eeabbe4134ce6c86d88a674921
이학준 <dlgkrwns213@naver.com>
2025-01-16 00:40:23 +0900

    Create Database Schema

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 import dotenv from "dotenv";
 import express from "express";
+import mainRouter from "./routes/main.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
+app.use("/", mainRouter);
 
 app.listen(port, () => {
   console.log(`Application listening on port ${port}`)
---
+++ config/db.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+import expressAsyncHandler from "express-async-handler";
+
+dotenv.config();
+
+const connectDB = expressAsyncHandler( async() => {
+  const connect  = await mongoose.connect(process.env.MONGODB_URL);
+  console.log(`DB Connected: ${connect.connection.host}`);
+});
+
+export default connectDB;
